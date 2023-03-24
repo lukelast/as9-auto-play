bc7ac19a88594c03dd9064d865de51f42eb8b004
Luke Last <llast@myriad.com>
2023-03-24 09:10:18 -0400

    Create screenshot capture directory if it does not exist.

--- src/as9/util/log.py
+++ src/as9/util/log.py
@@ -1,8 +1,7 @@
 import logging
 import sys
-
+import os
 import pyautogui
-
 from as9.util.constant import CAPTURE_DIR
 
 
@@ -11,4 +10,6 @@ def log_config():
                         format='%(asctime)s %(levelname)s %(message)s',
                         datefmt='%H:%M:%S',
                         stream=sys.stdout)
+    if not os.path.exists(CAPTURE_DIR):
+        os.makedirs(CAPTURE_DIR)
     pyautogui.screenshot(f"{CAPTURE_DIR}/start.png")
