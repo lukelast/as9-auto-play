1098461f939ccc532e02485ed48f3c50009bb5a9
Luke Last <llast@myriad.com>
2023-04-02 21:20:53 -0400

    comments

--- src/as9/util/settings.py
+++ src/as9/util/settings.py
@@ -6,10 +6,17 @@ def current_hunt():
     from as9.nav.base_hunt import HuntNagari
     return HuntNagari()
 
+
+""" Main logging level. """
 logging_level = logging.INFO
+
+""" How long to pause on launch, before starting to perform actions."""
 start_pause_sec = 4
+
 debug_save_ocr_images = False
 debug_save_screen_images = True
 
+
+# Constants.
 IMG_DIR = '../../../game-imgs'
 CAPTURE_DIR = '../../../captures'
